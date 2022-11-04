46a8009928949e90aaef8c86c7128d76e61229d0
Viktor <minkovvs@gmail.com>
2022-11-04 13:28:48 +0300

    feat:add code for 4 and 5 tasks

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const path = require('path');
+const fsPromise = require('fs/promises');
 
+
+const pathToOriginal = path.resolve(__dirname,'files')
+const pathToCopy = path.resolve(__dirname,'files-copy')
+// console.log(pathToOriginalFolder)
+
+const makeAndFillDir = async function (pathToOriginal,pathToCopy) {
+   const dir = await fsPromise.mkdir(pathToCopy,{recursive: true})
+   const files =  await fsPromise.readdir(pathToOriginal,{withFileTypes: true})
+
+   if(!dir) {
+      await fsPromise.rm(pathToCopy,{recursive: true})
+      await fsPromise.mkdir(pathToCopy)
+   }
+
+   files.forEach(file => {
+    let srcPath = path.join(pathToOriginal,file.name)
+    let destPath = path.join(pathToCopy,file.name)
+
+    fsPromise.copyFile(srcPath,destPath)
+   })
+}
+ makeAndFillDir(pathToOriginal,pathToCopy)
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const fs  = require('fs')
+const fsPromise = require('fs/promises')
+
+const pathToStyles = path.resolve(__dirname,'styles')
+const pathToBundle = path.resolve(__dirname,'project-dist','bundle.css')
+
+
+const mergeStyles = async function () {
+  try{
+    const styleFiles = await fsPromise.readdir(pathToStyles,{withFileTypes:true})
+    const bundle = fs.createWriteStream(pathToBundle);
+    let readableStream;
+
+    styleFiles.forEach(async (file) => {
+      const filePath = path.join(pathToStyles,file.name)
+      const ext = path.extname(filePath)
+
+      if(file.isFile() && ext === '.css') {
+        readableStream = fs.createReadStream(filePath)
+        readableStream.pipe(bundle)
+      }
+    })
+    readableStream.on('end',() => {
+      bundle.end()
+    })
+  }catch(err) {
+    console.log("ERORR =>",err)
+  }
+}
+
+mergeStyles()
